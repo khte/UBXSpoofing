e8e7c54a9f08be9f4e31b40c4fdd4ca0c01e3a9c
khte <khte@mmmi.sdu.dk>
2017-12-07 15:27:55 +0100

    Added function for extracting the baud rate desired by the FC and changing the baud rate for transmissions accordingly

--- spoofGPS_AQ.py
+++ spoofGPS_AQ.py
@@ -4,7 +4,8 @@ uBlox UBX protocol spoofer for AutoQuad
 Author: Kristian Husum Terkildsen, khte@mmmi.sdu.dk
 
 Notes to dev: 
-* Try other baud rates? 9600?
+* Test with Pixhawk2
+* Try timing the positional data sending
 * Add TIM-TP and AID-REQ?
 
 Sources of inspiration:
@@ -23,7 +24,7 @@ def readAndPrint(port):
 
 class UBXSpoofer():
 	def __init__(self):
-		self.ser0 = serial.Serial(port = "/dev/ttyUSB0") #, baudrate = 230400
+		self.serialOut = serial.Serial(port = "/dev/ttyUSB0") #, baudrate = 230400
 		#UBX header and ID's
 		self.SYNC = b"\xb5\x62"
 		self.NAV_VELNED = b"\x01\x12"
@@ -33,23 +34,24 @@ class UBXSpoofer():
 		self.messageClass = ""
 		self.messageID = ""
 		self.baudFound = False
+		self.debug = True
 	
 	def startSpoofing(self):
 		#Find the requested baud rate
 		while not self.baudFound:
-			readByte = self.ser0.read()
+			readByte = self.serialOut.read()
 			if readByte == "\xb5":
-				readBytes = self.ser0.read(3)
+				readBytes = self.serialOut.read(3)
 				if readBytes[1] == "\x06" and readBytes[2] == "\x00":
-					print "CFG-PRT"
-					readBytes = self.ser0.read(14)
-					print type(readBytes)
-					baudBytes = readBytes[10:]
-					print hex(ord(baudBytes[0])), hex(ord(baudBytes[1])), hex(ord(baudBytes[2])), hex(ord(baudBytes[3]))
-
-		#ser.baudrate = 115200
+					readBytes = self.serialOut.read(14)
+					foundBaudrate = ord(readBytes[10]) + ord(readBytes[11]) * 256 + ord(readBytes[12]) * 65536 + ord(readBytes[13]) * 16777216
+					self.serialOut.baudrate = foundBaudrate
+					self.baudFound = True
+					if self.debug == True:
+						print "CFG-PRT message received, port configured"
 		
 		#Send positional data to flight controller
+		print "Parsing positional data"
 		while True:
 			GPSMsToW = self.calcMsToW()
 			
@@ -82,25 +84,25 @@ class UBXSpoofer():
 	def sendNAV_VELNED(self, GPSMsToW, velN, velE, velD, speed, gSpeed, heading, sAcc, headingAcc):
 		msg = struct.pack('<cccchLlllLLlLL', self.SYNC[0], self.SYNC[1], self.NAV_VELNED[0], self.NAV_VELNED[1], 36, GPSMsToW, velN, velE, velD, speed, gSpeed, heading, sAcc, cAcc)
 		msg += self.checksum(msg[2:])
-		self.ser0.write(msg)
+		self.serialOut.write(msg)
 
 	"""Input units: ms, deg, deg, mm, mm, mm, mm"""
 	def sendNAV_POSLLH(self, GPSMsToW, lat, lon, height, hMSL, hAcc, vAcc): #Can GPSMsToW just be a constant?
 		msg = struct.pack('<cccchLllllLL', self.SYNC[0], self.SYNC[1], self.NAV_POSLLH[0], self.NAV_POSLLH[1], 28, GPSMsToW, lon * 10000000, lat * 10000000, height, hMSL, hAcc, vAcc)
 		msg += self.checksum(msg[2:])
-		self.ser0.write(msg)
+		self.serialOut.write(msg)
 	
 	"""Input units: ms"""
 	def sendNAV_DOP(self, GPSMsToW, gDOP, pDOP, tDOP, vDOP, hDOP, nDOP, eDOP):
 		msg = struct.pack('<cccchLHHHHHHH', self.SYNC[0], self.SYNC[1], self.NAV_DOP[0], self.NAV_DOP[1], 18, GPSMsToW, gDOP * 100, pDOP * 100, tDOP * 100, vDOP * 100, hDOP * 100, nDOP * 100, eDOP * 100)
 		msg += self.checksum(msg[2:])
-		self.ser0.write(msg)
+		self.serialOut.write(msg)
 	
 	"""Input units: ns, ns, year, month, day, hour, minute, second"""
 	def sendNAV_TIMEUTC(self, GPSMsToW, tAcc, nano, year, month, day, hour, minute, sec, valid):
 		msg = struct.pack('<cccchLLlHhhhhhc', self.SYNC[0], self.SYNC[1], self.NAV_TIMEUTC[0], self.NAV_TIMEUTC[1], 20, GPSMsToW, tAcc, nano, year, month, day, hour, minute, sec, valid)
 		msg += self.checksum(msg[2:])
-		self.ser0.write(msg)
+		self.serialOut.write(msg)
 	
 
 spoof = UBXSpoofer()
@@ -108,55 +110,55 @@ spoof.startSpoofing()
 
 """
 	def readMessagesStream(self):
-		readByte = self.ser0.read()
+		readByte = self.serialOut.read()
 		while True:
 			if readByte == "\xb5":
 				print "----------NEW MESSAGE----------"
 				print hex(ord(readByte))
-				readAndPrint(self.ser0)
+				readAndPrint(self.serialOut)
 				print "-----------CLASS & ID----------"
-				readAndPrint(self.ser0)
-				readAndPrint(self.ser0)
+				readAndPrint(self.serialOut)
+				readAndPrint(self.serialOut)
 				print "------------LENGTH-------------"
-				readAndPrint(self.ser0)
-				readAndPrint(self.ser0)
+				readAndPrint(self.serialOut)
+				readAndPrint(self.serialOut)
 				print "------PAYLOAD & CHECKSUM-------"
-				readByte = self.ser0.read() #Reads startbyte of next package
+				readByte = self.serialOut.read() #Reads startbyte of next package
 				while readByte != "\xb5":
 					print hex(ord(readByte))
-					readByte = self.ser0.read()
+					readByte = self.serialOut.read()
 				print "----------MESSAGE END----------"
 			else:
-				readByte = self.ser0.read()
+				readByte = self.serialOut.read()
 				print hex(ord(readByte))
 				
 	def classify(self):
 		readByte = ""
 		while readByte != "\xb5":
-			readByte = self.ser0.read()
-		readByte = self.ser0.read()
-		self.messageClass = self.ser0.read()
-		self.messageID = self.ser0.read()
+			readByte = self.serialOut.read()
+		readByte = self.serialOut.read()
+		self.messageClass = self.serialOut.read()
+		self.messageID = self.serialOut.read()
 		print "----------Message classified-----------"
 		print "messageClass: ", hex(ord(self.messageClass))
 		print "messageID: ", hex(ord(self.messageID))
 
 	def sendACK(self):
 		MSG = ""
-		self.ser0.write(struct.pack('c', b"\xb5"))
-		self.ser0.write(struct.pack('c', b"\x62"))
-		self.ser0.write(struct.pack('c', b"\x05"))
+		self.serialOut.write(struct.pack('c', b"\xb5"))
+		self.serialOut.write(struct.pack('c', b"\x62"))
+		self.serialOut.write(struct.pack('c', b"\x05"))
 		MSG = struct.pack('c', b"\x05")
-		self.ser0.write(struct.pack('c', b"\x01"))
+		self.serialOut.write(struct.pack('c', b"\x01"))
 		MSG += struct.pack('c', b"\x01")
-		self.ser0.write(struct.pack('<h', 2))
+		self.serialOut.write(struct.pack('<h', 2))
 		MSG += struct.pack('<h', 2)
-		self.ser0.write(struct.pack('c', self.messageClass))
+		self.serialOut.write(struct.pack('c', self.messageClass))
 		MSG += struct.pack('c', self.messageClass)
-		self.ser0.write(struct.pack('c', self.messageID))
+		self.serialOut.write(struct.pack('c', self.messageID))
 		MSG += struct.pack('c', self.messageID)
 		A, B = self.checksum(MSG)
-		self.ser0.write(A)
-		self.ser0.write(B)
+		self.serialOut.write(A)
+		self.serialOut.write(B)
 		print "ACK sent"
 """
